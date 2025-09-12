8da8339fa5d04196755ab7ee3f1492befa02ac79
JeevithaDS <jeevithads10@gmail.com>
2025-09-13 05:00:50 +0530

    added dfs

--- 7.DFSMatrix.c
+++ 7.DFSMatrix.c
@@ -1,116 +1,131 @@
-/* program to implement quick sort*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-int count;
+int isCycle = 0, components = 0, n, count = 0, isTester = 0;
+int d;
 
-void swap(int *a, int *b)
+void dfs(int mat[n][n], int *vis,int *path, int source, int par)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+    vis[source] = 1;
 
-int partition(int *arr, int beg, int end)
-{
-    int pivot = arr[beg];
-    int i = beg, j = end + 1;
-    do
+    if (isTester)
+        printf("%c ", source+65);
+
+    for (int i = 0; i < n; i++)
     {
-        do
+        count++;
+        if(d==1)
         {
-            count++;
-            i++;
-        } while (arr[i] < pivot);
-        do
+         if (mat[source][i] && vis[i] && i != par && path[i])
+        
+            isCycle = 1;
+        
+        }
+       
+        else (mat[source][i] && vis[i] && i != par)
         {
-            count++;
-            j--;
-        } while (arr[j] > pivot);
-        swap(&arr[i], &arr[j]);
-    } while (i < j);
-    swap(&arr[i], &arr[j]);
-    swap(&arr[beg], &arr[j]);
-    return j;
-}
+            isCycle = 1;
+            
+        }
+        if(mat[source][i] && !vis[i])
+        {
+            dfs(mat, vis, path,i, source);
 
-void quicksort(int *arr, int beg, int end)
-{
-    if (beg < end)
-    {
-        int split = partition(arr, beg, end);
-        quicksort(arr, beg, split - 1);
+        }
+        path[source]=0;
 
-        quicksort(arr, split + 1, end);
     }
+}
+
+void checkConnectivity(int mat[n][n])
+{
+    int vis[n];
+    int path[n];
+    int k = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        vis[i] = 0;
+        path[i]=0;
+    }
+
+    for (int i = 0; i < n; i++)
+        if (!vis[i])
+        {
+            components++;
+
+            if (isTester)
+                printf("\nComponent %d: ", k++);
+            dfs(mat, &vis[0], &path[0],i, -1);
+        }
 }
 
 void tester()
 {
-    int *arr, n;
-    printf("ENTER THE NUMBER OF ELEMENTS\n");
+    isTester = 1;
+    printf("Enter the number of vertices\n");
     scanf("%d", &n);
-    arr = (int *)malloc(sizeof(int) * n);
-    printf("ENTER THE ELEMENTS OF THE ARRAY\n");
+    int adjMat[n][n];
+    printf("Enter the adjacency matrix :\n");
     for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-    printf("THE ELEMENTS OF THE ARRAY BEFORE SORTING\n");
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-    quicksort(arr, 0, n - 1);
-    printf("THE ELEMENTS OF THE ARRAY BEFORE SORTING\n");
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-    printf("\n");
+        for (int j = 0; j < n; j++)
+            scanf("%d", &adjMat[i][j]);
+    printf("\nEnter directed or not:");
+    scanf("%d",&d);        
+
+    checkConnectivity(adjMat);
+    if(count==n)
+    {
+        printf("\nThe graph is connected\n");
+    }
+    else
+    {
+         printf("\nThe graph is connected\n");
+
+    }
+
+    printf("\nThe number of connected components :%d\n", components);
+
+    if (isCycle)
+        printf("Cycle exists\n");
+    else
+        printf("Cycle doesnot exists\n");
 }
 
 void plotter()
 {
-    int *arr, n;
-    srand(time(NULL));
-    FILE *f1, *f2, *f3;
-    f1 = fopen("Quickbest.txt", "w");
-    f2 = fopen("Quickworst.txt", "w");
-    f3 = fopen("Quickavg.txt", "w");
-    n = 4;
-    while (n <= 1024)
+    FILE *f1 = fopen("dfsadjMat.txt", "w");
+    isTester = 0;
+
+    for (int k = 1; k <= 10; k++)
     {
-        arr = (int *)malloc(sizeof(int) * n);
+        n = k;
+        int adjMat[n][n];
+
         for (int i = 0; i < n; i++)
-            *(arr + i) = 5;
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i != j)
+                    adjMat[i][j] = 1;
+                else
+                    adjMat[i][j] = 0;
+            }
+        }
         count = 0;
-        // Best case
-        quicksort(arr, 0, n - 1);
-        fprintf(f1, "%d\t%d\n", n, count); // printf("%d\t%d\n",n,count);
-        // worst case
-        count = 0;
-        for (int i = 0; i < n; i++)
-            *(arr + i) = i + 1;
-        quicksort(arr, 0, n - 1);
-        fprintf(f2, "%d\t%d\n", n, count); // printf("%d\t%d\n",n,count);
-        // AVG case
-        for (int i = 0; i < n; i++)
-            *(arr + i) = rand() % n;
-        count = 0;
-        quicksort(arr, 0, n - 1);
-        fprintf(f3, "%d\t%d\n", n, count); // printf("%d\t%d\n",n,count);
-        n = n * 2;
-        free(arr);
+        checkConnectivity(adjMat);
+        fprintf(f1, "%d\t%d\n", n, count);
     }
     fclose(f1);
-    fclose(f2);
-    fclose(f3);
 }
 
 void main()
 {
-    int ch;
-    printf("Enter your choice: \n\n1.Tester\n2.Plotter\n\n");
-    scanf("%d", &ch);
-    switch (ch)
+    int choice;
+    printf("Enter\n1.Tester\n2.Plotter\n");
+    scanf("%d", &choice);
+
+    switch (choice)
     {
     case 1:
         tester();
@@ -119,6 +134,6 @@ void main()
         plotter();
         break;
     default:
-        printf("Invalid choice!!!\n\n");
+        printf("Invalid choice");
     }
 }
